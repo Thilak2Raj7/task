0be4355be14c8fd3ae33dafb8071651634c620c4
Thilak2Raj7 <95743344+Thilak2Raj7@users.noreply.github.com>
2022-08-30 18:47:10 +0530

    Add files via upload

--- FirstDemo/src/demoProject/First.java
+++ FirstDemo/src/demoProject/First.java
@@ -1,21 +1,44 @@
 package demoProject;
 
+import java.util.Iterator;
+import java.util.List;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.By.ByName;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class First {
 
-	public static void main(String[] args)
-	{
-		System.setProperty("webdriver.chrome.driver","C:\\Users\\Administrator\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
-		WebDriver driver=new ChromeDriver();
-		//driver.get("https://demo.servicedeskplus.com/app#/admin/modules/priority");
-		driver.navigate().to("https://demo.servicedeskplus.com/app#/admin/modules/priority");
-		driver.findElement(By.className("per50")).click();
-        //driver.navigate().forward();
-		driver.findElement(By.className("truncate-wrapper")).click();
-		driver.findElement(By.xpath("//*[name()='svg']//*[local-name()='header-menu-icons']")).click();
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\Administrator\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
 		
+		driver.navigate().to("https://demo.servicedeskplus.com");
+		driver.findElement(By.xpath("//*[@id='scroll']/div/div[2]/div[1]/a/img")).click();
+		driver.findElement(By.xpath("//*[@id='esmPortalHome']/div[2]/div[1]/div/div[1]/div/div[1]/img")).click();
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		driver.findElement(By.id("admin-hi")).click();
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		driver.findElement(By.xpath("//*[@id='admin-landing-page']/div[1]/div[1]/div/div[2]/div/input")).sendKeys("Priority");
+		driver.findElement(By.xpath("//*[@id='select2-result-label-357']/a")).click();
+        driver.findElement(By.id("add_new_priorities")).click();
+        driver.findElement(By.id("priorities_name")).sendKeys("First");
+        driver.findElement(By.id("saveform")).click();
 	}
+
 }
